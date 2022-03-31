c862d39d92709fa347eb00ed6d19061386a3789f
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 18:02:02 +0300

    Search Action updated.

--- ProjectOne/Menu/Actions/SearchPhone.cs
+++ ProjectOne/Menu/Actions/SearchPhone.cs
@@ -11,15 +11,69 @@ namespace ProjectOne.Menu.Actions
         public static void Search(List<Person> listOfPeople)
         {
             Console.WriteLine("**********************************************");
-            Console.WriteLine("Aranacak kişinin ismini seçiniz!");
+            Console.WriteLine("Aranacak istediğiniz tipi seçiniz!");
+            Console.WriteLine("Aranacak istediğiniz tipi seçiniz!");
+            Console.WriteLine("İsim veya soyisime göre arama yapmak için: (1)");
+            Console.WriteLine("Telefon numarasına göre arama yapmak için: (2)");
+
+            var searchType = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if(searchType == 1)
+            {
+                Console.Write("Aramak istediğiniz ismi veya soyismi giriniz: ");
+                var nameToSearch = Convert.ToString(Console.ReadLine().Trim());
+                Console.WriteLine("--");
+
+                var searchedPeoplebyName = listOfPeople.Where(p => p.Name == nameToSearch || p.Surname == nameToSearch);
+
+                if (searchedPeoplebyName.Count() == 0)
+                {
+                    Console.WriteLine("Uygun kriterlerde kişi bulunamadı.");
+                    Console.WriteLine("--");
+                }
 
 
-            var nameToSearch = Convert.ToString(Console.ReadLine().Trim());
-            var searchedPerson= listOfPeople.Where(p => p.Name == nameToSearch).SingleOrDefault();
-               
-            Console.WriteLine("İsim : " + searchedPerson.Name);
-            Console.WriteLine("Soyisim : " + searchedPerson.Surname);
-            Console.WriteLine("Telefon : " + searchedPerson.PhoneNumber);
+                foreach (var p in searchedPeoplebyName)
+                {
+                    Console.WriteLine("İsim : " + p.Name);
+                    Console.WriteLine("Soyisim : " + p.Surname);
+                    Console.WriteLine("Telefon : " + p.PhoneNumber);
+                    Console.WriteLine("--");
+                }
+
+                
+            }
+            else if(searchType == 2)
+            {
+                Console.Write("Aramak istediğiniz telefonu giriniz: ");
+                var phoneToSearch = Convert.ToInt32(Console.ReadLine().Trim());
+                Console.WriteLine("--");
+
+                var searchedPeoplebyPhone = listOfPeople.Where(p => p.PhoneNumber == phoneToSearch);
+
+                if(searchedPeoplebyPhone.Count() == 0)
+                {
+                    Console.WriteLine("Uygun kriterlerde kişi bulunamadı.");
+                    Console.WriteLine("--");
+                }
+
+                foreach (var p in searchedPeoplebyPhone)
+                {
+                    Console.WriteLine("İsim : " + p.Name);
+                    Console.WriteLine("Soyisim : " + p.Surname);
+                    Console.WriteLine("Telefon : " + p.PhoneNumber);
+                    Console.WriteLine("--");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Yanlış seçim yaptınız. Yeniden Deneyiniz.");
+                Menu.Actions.SearchPhone.Search(listOfPeople);
+            }
+
+
+
         }
     }
 }

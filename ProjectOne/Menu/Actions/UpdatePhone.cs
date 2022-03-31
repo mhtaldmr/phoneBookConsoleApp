a3b6245834d3194cb30aef5be2eddc9c31cd48db
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 16:27:22 +0300

    Update Action Added

--- ProjectOne/Menu/Actions/UpdatePhone.cs
+++ ProjectOne/Menu/Actions/UpdatePhone.cs
@@ -10,6 +10,24 @@ namespace ProjectOne.Menu.Actions
     {
         public static void Update(List<Person> listOfPeople)
         {
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("Güncellenecek kişinin ismini seçiniz!");
+            var nameToUpdate = Convert.ToString(Console.ReadLine().Trim());
+
+            var personToUpdate = listOfPeople.Where(p => p.Name == nameToUpdate).SingleOrDefault();
+
+            Console.WriteLine("Güncellemek istediğiniz bilgileri giriniz.");
+            Console.Write("Lütfen isim giriniz:");
+            personToUpdate.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Lütfen soyisim giriniz:");
+            personToUpdate.Surname = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Lütfen telefon no giriniz:");
+            personToUpdate.PhoneNumber = Convert.ToInt32(Console.ReadLine().Trim());
+
+
+            Console.WriteLine(personToUpdate.Name + " isimli kişi güncellenmiştir.\n");
 
         }
     }

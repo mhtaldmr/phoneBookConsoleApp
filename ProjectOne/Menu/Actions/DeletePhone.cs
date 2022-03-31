39ed02dd58b7a4cf381efc08eaeb096107d1e37f
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 17:13:53 +0300

    Delete action updated

--- ProjectOne/Menu/Actions/DeletePhone.cs
+++ ProjectOne/Menu/Actions/DeletePhone.cs
@@ -11,12 +11,41 @@ namespace ProjectOne.Menu.Actions
         public static void Delete(List<Person> listOfPeople)
         {
             Console.WriteLine("**********************************************");
-            Console.WriteLine("Silinecek kişinin ismini seçiniz!");
+            Console.Write("Silinecek kişinin ismini seçiniz: ");
             var nameToDelete = Convert.ToString(Console.ReadLine().Trim());
 
-            var personToDelete = listOfPeople.Where(p => p.Name == nameToDelete).SingleOrDefault();
-            listOfPeople.Remove(personToDelete);
-            Console.WriteLine(personToDelete.Name + " isimli kişi silinmiştir.\n");
+            Console.Write("Silinecek kişinin soy ismini seçiniz: ");
+            var surnameToDelete = Convert.ToString(Console.ReadLine().Trim());
+
+            var personToDelete = listOfPeople.Where(p => p.Name == nameToDelete && p.Surname == surnameToDelete).FirstOrDefault();
+            
+            if (personToDelete != null)
+            {
+                Console.WriteLine(personToDelete.Name + " " + personToDelete.Surname + " isimli kişi rehberden silinmek üzere, onaylıyor musunuz ?(y/n)");
+                var validation = Convert.ToString(Console.ReadLine().Trim());
+                if(validation == "y")
+                {
+                    listOfPeople.Remove(personToDelete);
+                    Console.WriteLine(personToDelete.Name + " isimli kişi silinmiştir.\n");
+                }else if(validation == "n")
+                {
+                    Menu.Actions.DeletePhone.Delete(listOfPeople);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Belirtilen isimli kişi bulunamamamıştır.\n");
+                Console.WriteLine("Silmeyi Sonlandırmak için : (1)");
+                Console.WriteLine("Yeniden denemek için : (2)");
+                var lastSelection = Convert.ToInt32(Console.ReadLine().Trim());
+                if(lastSelection == 1)
+                {
+                    Console.WriteLine("Silme Sonlandırılmıştır");
+                }else if(lastSelection == 2)
+                {
+                    Menu.Actions.DeletePhone.Delete(listOfPeople);
+                }
+            }
 
 
         }

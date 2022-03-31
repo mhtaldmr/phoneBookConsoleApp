1bb74349e316cc4c8780b00e35a807cfcca7388c
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 15:38:57 +0300

    Search Action added

--- ProjectOne/Menu/Actions/SearchPhone.cs
+++ ProjectOne/Menu/Actions/SearchPhone.cs
@@ -10,7 +10,16 @@ namespace ProjectOne.Menu.Actions
     {
         public static void Search(List<Person> listOfPeople)
         {
-            
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("Aranacak kişinin ismini seçiniz!");
+
+
+            var nameToSearch = Convert.ToString(Console.ReadLine().Trim());
+            var searchedPerson= listOfPeople.Where(p => p.Name == nameToSearch).SingleOrDefault();
+               
+            Console.WriteLine("İsim : " + searchedPerson.Name);
+            Console.WriteLine("Soyisim : " + searchedPerson.Surname);
+            Console.WriteLine("Telefon : " + searchedPerson.PhoneNumber);
         }
     }
 }

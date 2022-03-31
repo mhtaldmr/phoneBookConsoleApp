cc913e78b366f3348948a5fd9e6a85b4af8a343d
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 17:27:23 +0300

    Update Action updated.

--- ProjectOne/Menu/Actions/UpdatePhone.cs
+++ ProjectOne/Menu/Actions/UpdatePhone.cs
@@ -11,23 +11,50 @@ namespace ProjectOne.Menu.Actions
         public static void Update(List<Person> listOfPeople)
         {
             Console.WriteLine("**********************************************");
-            Console.WriteLine("Güncellenecek kişinin ismini seçiniz!");
+            Console.Write("Güncellenecek kişinin ismini yazınız: ");
             var nameToUpdate = Convert.ToString(Console.ReadLine().Trim());
 
-            var personToUpdate = listOfPeople.Where(p => p.Name == nameToUpdate).SingleOrDefault();
+            Console.Write("Güncellenecek kişinin soy ismini yazınız: ");
+            var surnameToUpdate = Convert.ToString(Console.ReadLine().Trim());
 
-            Console.WriteLine("Güncellemek istediğiniz bilgileri giriniz.");
-            Console.Write("Lütfen isim giriniz:");
-            personToUpdate.Name = Convert.ToString(Console.ReadLine());
-
-            Console.Write("Lütfen soyisim giriniz:");
-            personToUpdate.Surname = Convert.ToString(Console.ReadLine());
-
-            Console.Write("Lütfen telefon no giriniz:");
-            personToUpdate.PhoneNumber = Convert.ToInt32(Console.ReadLine().Trim());
+            var personToUpdate = listOfPeople.Where(p => p.Name == nameToUpdate && p.Surname == surnameToUpdate).FirstOrDefault();
 
 
-            Console.WriteLine(personToUpdate.Name + " isimli kişi güncellenmiştir.\n");
+            if (personToUpdate != null)
+            {
+                Console.WriteLine("Güncellemek istediğiniz bilgileri giriniz.");
+                Console.Write("Lütfen isim giriniz:");
+                personToUpdate.Name = Convert.ToString(Console.ReadLine());
+
+                Console.Write("Lütfen soyisim giriniz:");
+                personToUpdate.Surname = Convert.ToString(Console.ReadLine());
+
+                Console.Write("Lütfen telefon no giriniz:");
+                personToUpdate.PhoneNumber = Convert.ToInt32(Console.ReadLine().Trim());
+
+                Console.WriteLine(personToUpdate.Name + " isimli kişi güncellenmiştir.\n");
+            }
+            else
+            {
+                Console.WriteLine("Belirtilen isimli kişi bulunamamamıştır.\n");
+                Console.WriteLine("Güncellemeyi Sonlandırmak için : (1)");
+                Console.WriteLine("Yeniden denemek için : (2)");
+                var lastSelection = Convert.ToInt32(Console.ReadLine().Trim());
+                if (lastSelection == 1)
+                {
+                    Console.WriteLine("Güncellendirme Sonlandırılmıştır");
+                }
+                else if (lastSelection == 2)
+                {
+                    Menu.Actions.UpdatePhone.Update(listOfPeople);
+                }
+            }
+
+
+
+
+
+
 
         }
     }
